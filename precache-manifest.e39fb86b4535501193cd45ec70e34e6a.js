082ca53dfa7e434094c551923097861e9b436d73
Michal Majer <majer.michal@outlook.com>
2019-05-03 10:31:28 +1000

    Updates

---
+++ precache-manifest.e39fb86b4535501193cd45ec70e34e6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1aae78b5c64c885b46d82639b2080c1",
+    "url": "/Weather_app/index.html"
+  },
+  {
+    "revision": "193138d0a0d21106f412",
+    "url": "/Weather_app/static/css/main.79ade46a.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Weather_app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "193138d0a0d21106f412",
+    "url": "/Weather_app/static/js/main.02a5a336.chunk.js"
+  },
+  {
+    "revision": "ee2a57cb877331e77299",
+    "url": "/Weather_app/static/js/runtime~main.3034a4a1.js"
+  }
+]);
